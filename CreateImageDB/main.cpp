dd90f519b4b5d63b007dada70089563c16a31afe
yuuki <uw.qualia@icloud.com>
2017-11-24 11:56:21 +0900

    Initial Commit

---
+++ CreateImageDB/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CreateImageDB
+//
+//  Created by 室川優希 on 2017/11/24.
+//  Copyright © 2017年 室川優希. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
